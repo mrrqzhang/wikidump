853721ea00b1cfce81d25cded2d1d1341a3192c6
ruiqiang <ruiqiang@yahoo-inc.com>
2016-09-15 20:41:50 +0000

    write sql format

---
+++ write-sql-db.pig
@@ -0,0 +1,14 @@
+
+-- eid, caption, alias, creation_time, creator, suggestion, description
+
+DEFINE DBFORMAT `python write-sql-db.py` ship('/tmp/ruiqiang/wikidump/write-sql-db.py','/homes/ruiqiang/MyPythonLib/mypythonlib.py'); ;
+
+%default input /projects/qrw/ruiqiang/wiki/entity/
+
+data = load '$input' using PigStorage('\t','-noschema') ; ;
+
+entitytable = stream data through DBFORMAT as (eid:chararray, caption:chararray, alias:chararray, creation_time:chararray, creator:chararray, suggestion:chararray, description:chararray) ;
+
+rmf /projects/qrw/ruiqiang/wikisqldb
+store entitytable into '/projects/qrw/ruiqiang/wikisqldb' using PigStorage('\t','-schema') ;
+
--- write-sql-db.py
+++ write-sql-db.py
@@ -1,9 +1,21 @@
-import sys,re
+import sys,re,string
 
-from mypythonlib import remove_punct
+sys.path.insert(0,'/'.join(__file__.split('/')[:-1]))
+
 #schema
 #eid, caption, alias, creation_time, creator, suggestion, description
+
+def remove_punct(sent):
+    regex = re.compile(r'([%s])' % re.escape(string.punctuation))
+    sent = regex.sub(r' ', sent)
+    sent = re.sub('\s+',' ',sent)
+    sent = re.sub('\s+$','',sent)
+    sent = re.sub('^\s+','',sent)
+    return sent
+
+
 for line in sys.stdin:
+  try:
     fields = line.strip('\r\t\n').split('\t')
     if len(fields)<2: continue
     eid = fields[0]
@@ -15,4 +27,4 @@ for line in sys.stdin:
     suggestion = '|'.join( re.split(' ',temp))
     description = '.'.join(re.split('\.',fields[1])[0:2]) + '.'
     sys.stdout.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (eid, caption,alias,creation_time,creator,suggestion,description))
-
+  except: pass
